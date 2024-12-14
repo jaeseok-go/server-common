07e070994311ee18007e9390491c96d11f3fe956
jaeseok-go <gojeasuk2@naver.com>
2024-12-14 22:13:49 +0900

    test: CommonException 팩토리 메서드 생성 테스트 작성

--- common-utils/src/main/java/kr/js/commonutils/mvc/exception/CommonException.java
+++ common-utils/src/main/java/kr/js/commonutils/mvc/exception/CommonException.java
@@ -9,16 +9,12 @@ public class CommonException extends RuntimeException {
 
     private final String errorCode;
 
-    private CommonException(String errorCode) {
-        this(errorCode, "예외 메세지가 입력되지 않았습니다.");
-    }
-
     private CommonException(String errorCode, String message) {
         super(message);
         this.errorCode = errorCode;
     }
 
-    public CommonException of(String errorCode, String message) {
+    public static CommonException of(String errorCode, String message) {
         return new CommonException(errorCode, message);
     }
 }
---
+++ common-utils/src/test/java/kr/js/commonutils/mvc/exception/CommonExceptionTest.java
@@ -0,0 +1,22 @@
+package kr.js.commonutils.mvc.exception;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommonExceptionTest {
+
+    @DisplayName("유효한 에러코드와 메세지로 예외 생성 시 정상적인 예외 생성")
+    @Test
+    void ErrorCodeAndMessageIsValid_ConstructByFactoryMethod_ValidException() {
+        String errorCode = "ERROR_CODE";
+        String message = "MESSAGE";
+
+        CommonException exception = CommonException.of(errorCode, message);
+
+        assertEquals(errorCode, exception.getErrorCode());
+        assertEquals(message, exception.getMessage());
+    }
+
+}
